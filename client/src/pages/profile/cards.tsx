d413fa7b974f34ea4a1348c137adca8e69aa2a60
SrAmaral <edu.amaral8@gmail.com>
2021-10-16 21:04:24 -0300

    Section 30 Module 3 Finished

---
+++ client/src/pages/profile/cards.tsx
@@ -0,0 +1,21 @@
+import CardsList, { CardsListProps } from 'components/CardsList'
+import React from 'react'
+import Profile from 'templates/Profile'
+
+import mockCards from 'components/PaymentOptions/mock'
+
+export default function Cards({ cards }: CardsListProps) {
+  return (
+    <Profile>
+      <CardsList cards={cards} />
+    </Profile>
+  )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      cards: mockCards
+    }
+  }
+}
